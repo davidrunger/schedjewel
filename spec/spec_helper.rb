3183ea2effdd71444967687791a7efae05c74297
David Runger <davidjrunger@gmail.com>
2025-02-05 06:16:39 -0600

    Remove manual require of the gem itself (#387)
    This is not needed, because we have `Bundler.require(:default, :test)`,
    and we have `gemspec` in the default group of the `Gemfile`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,6 @@ end
 require 'bundler/setup'
 Bundler.require(:default, :test)
 
-require_relative '../lib/schedjewel.rb'
-
 Dir['spec/support/**/*.rb'].each { |file| require("./#{file}") }
 require 'active_support'
 require 'active_support/testing/time_helpers'
